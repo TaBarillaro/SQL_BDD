6229a7e1cff58b354b15a5a34de34001ba3e9aa6
Salome-Burteaux <salome.burteaux@le-campus-numerique.fr>
2025-02-07 15:00:06 +0100

    games created

---
+++ queries/insertion.sql
@@ -0,0 +1,5 @@
+-- Active: 1738662623843@@127.0.0.1@6603@projet2fou
+INSERT INTO game_list (name_, size, boardgame_size, boardgame_display_size, boardgame_color, pawn_color, pawn_image, pawn_pattern)
+VALUES ('Tictactoe', 3 , 3, 100, "green", "yellow", "image.png", "pattern.png"),
+('GOMOKU', 15 , 15, 100, "green", "yellow", "image.png", "pattern.png"),
+('Puissance 4', 7, 7, 100, "green", "yellow", "image.png", "pattern.png");
