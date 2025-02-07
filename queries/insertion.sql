541ee6fe972a77e9e0f2cdd458967b4662d0cb02
TaBarillaro <tania.barillaro@le-campus-numerique.fr>
2025-02-07 14:59:18 +0100

    first user created

---
+++ queries/insertion.sql
@@ -0,0 +1,3 @@
+INSERT INTO User_ (firstname, lastname, username, email, password, birthday, created_at, has_token, is_active, is_online, img_avatar, last_connection)  
+VALUES ('John', 'Doe', 'Jojo', 'john.doe@test.com', SHA2('mot_de_passe', 256), '1999-02-05', '2025-02-07 11:11:11', TRUE, TRUE, TRUE, 'image/homme-affaires.avif',
+'2025-02-07 14:00:30');
